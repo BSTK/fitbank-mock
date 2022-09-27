ae84b785d57889b7c37a8985963e037987a35c6f
Bruno Luz <bsilva.se@gmail.com>
2022-09-27 14:44:48 -0300

    Adicionando casos de teste para CreateAccountServiceTest

--- src/test/java/com/br/sellers/fitbank/mock/service/account/CreateAccountServiceTest.java
+++ src/test/java/com/br/sellers/fitbank/mock/service/account/CreateAccountServiceTest.java
@@ -1,5 +1,6 @@
 package com.br.sellers.fitbank.mock.service.account;
 
+import com.br.sellers.fitbank.helper.ServiceValidateFieldsHelper;
 import com.br.sellers.fitbank.mock.gateway.model.request.account.CreateAccountRequestModel;
 import com.br.sellers.fitbank.mock.gateway.model.response.BasicResponseModel;
 import com.br.sellers.fitbank.mock.gateway.model.response.account.CreateAccountResponseModel;
@@ -8,6 +9,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 class CreateAccountServiceTest {
 
@@ -23,6 +26,94 @@ class CreateAccountServiceTest {
         Assertions.assertNotNull(response);
         Assertions.assertEquals(CreateAccountResponseModel.class, response.getClass());
         Assertions.assertNull(response.getValidation());
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void shouldReturnFieldValidationErrorMail(final String email) throws JsonProcessingException {
+        final CreateAccountRequestModel request = request();
+        request.setMail(email);
+
+        final BasicResponseModel response = createAccountService.execute(GSON.toJson(request), SIMULATE_SCENARIO);
+
+        ServiceValidateFieldsHelper.executeAssertionsValidateFields("Mail", response);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void shouldReturnFieldValidationErrorNickname(final String email) throws JsonProcessingException {
+        final CreateAccountRequestModel request = request();
+        request.setNickname(email);
+
+        final BasicResponseModel response = createAccountService.execute(GSON.toJson(request), SIMULATE_SCENARIO);
+
+        ServiceValidateFieldsHelper.executeAssertionsValidateFields("Nickname", response);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void shouldReturnFieldValidationErrorName(final String email) throws JsonProcessingException {
+        final CreateAccountRequestModel request = request();
+        request.setName(email);
+
+        final BasicResponseModel response = createAccountService.execute(GSON.toJson(request), SIMULATE_SCENARIO);
+
+        ServiceValidateFieldsHelper.executeAssertionsValidateFields("Name", response);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void shouldReturnFieldValidationErrorTaxNumber(final String email) throws JsonProcessingException {
+        final CreateAccountRequestModel request = request();
+        request.setTaxNumber(email);
+
+        final BasicResponseModel response = createAccountService.execute(GSON.toJson(request), SIMULATE_SCENARIO);
+
+        ServiceValidateFieldsHelper.executeAssertionsValidateFields("TaxNumber", response);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void shouldReturnFieldValidationErrorBank(final String email) throws JsonProcessingException {
+        final CreateAccountRequestModel request = request();
+        request.setBank(email);
+
+        final BasicResponseModel response = createAccountService.execute(GSON.toJson(request), SIMULATE_SCENARIO);
+
+        ServiceValidateFieldsHelper.executeAssertionsValidateFields("Bank", response);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void shouldReturnFieldValidationErrorBankBranch(final String email) throws JsonProcessingException {
+        final CreateAccountRequestModel request = request();
+        request.setBankBranch(email);
+
+        final BasicResponseModel response = createAccountService.execute(GSON.toJson(request), SIMULATE_SCENARIO);
+
+        ServiceValidateFieldsHelper.executeAssertionsValidateFields("BankBranch", response);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void shouldReturnFieldValidationErrorBankAccount(final String email) throws JsonProcessingException {
+        final CreateAccountRequestModel request = request();
+        request.setBankAccount(email);
+
+        final BasicResponseModel response = createAccountService.execute(GSON.toJson(request), SIMULATE_SCENARIO);
+
+        ServiceValidateFieldsHelper.executeAssertionsValidateFields("BankAccount", response);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void shouldReturnFieldValidationErrorBankAccountDigit(final String email) throws JsonProcessingException {
+        final CreateAccountRequestModel request = request();
+        request.setBankAccountDigit(email);
+
+        final BasicResponseModel response = createAccountService.execute(GSON.toJson(request), SIMULATE_SCENARIO);
+
+        ServiceValidateFieldsHelper.executeAssertionsValidateFields("BankAccountDigit", response);
     }
 
     private CreateAccountRequestModel request() {
